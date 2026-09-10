9b90197112b89a60986f9e4b7b758251ac83bf48
jhs <3194585837@qq.com>
2026-09-10 22:00:17 +0800

    update build.sh
    Signed-off-by: jhs <3194585837@qq.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-NUTTX_DIR=$(pwd)/nuttx
-# cmake --build build -t clean
-cd build
-cmake $NUTTX_DIR -DBOARD_CONFIG=stm32f103-alientek:nsh -GNinja
-ninja
-size nuttx
+cmake -S nuttx -B build -DBOARD_CONFIG=stm32f103-alientek:nsh -GNinja
+cmake --build build --parallel
+size build/nuttx
