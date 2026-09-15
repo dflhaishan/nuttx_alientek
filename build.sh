74e19117551f29dbb1f82109a9f49969310c3fdf
jhs <3194585837@qq.com>
2026-09-15 13:51:40 +0800

    source for nuttx_os
    Signed-off-by: jhs <3194585837@qq.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cmake -S nuttx -B build -DBOARD_CONFIG=stm32f103-alientek:nsh -GNinja
+cmake -S nuttx_os -B build -DBOARD_CONFIG=stm32f103-alientek:nsh -GNinja
 cmake --build build --parallel
+#cmake --install build --prefix build/staging/
 size build/nuttx
