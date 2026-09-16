ad249239995f6cf125f1250bb9d8fed815841ff1
jhs <3194585837@qq.com>
2026-09-16 16:48:25 +0800

    fully linked elf
    Signed-off-by: jhs <3194585837@qq.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cmake -S nuttx_os -B build -DBOARD_CONFIG=stm32f103-alientek:nsh -GNinja
 cmake --build build --parallel
-#cmake --install build --prefix build/staging/
+# cmake --install build --prefix build/staging/
+# ./nuttx_os/tools/build/mksymtab -d nuttx_os/libs/libc/libc.csv /home/jhs/nuttx/nuttx_os/boards/arm/stm32/stm32f103-alientek/src/generated_symtab.c
 size build/nuttx
